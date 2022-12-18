6993044493ba2e8cb528174847d82a2e188d8f07
Boris I. Bendovsky <bibendovsky@hotmail.com>
2022-12-18 19:55:43 +0200

    Add custom CRT infrastructure

--- src/eaxefx_app_patcher/src/eaxefx_app_patcher.c
+++ src/eaxefx_app_patcher/src/eaxefx_app_patcher.c
@@ -24,12 +24,632 @@ OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
 
-#define _CRT_SECURE_NO_WARNINGS
-
+#ifdef EAXEFX_APP_PATCHER_CUSTOM_CRT
+#include <stdarg.h>
+#else
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#endif
+
+/*=========================================================================*/
+
+#ifdef EAXEFX_APP_PATCHER_CUSTOM_CRT
+
+#if defined(_WIN32)
+
+/* <<< WINAPI */
+
+#define WINAPI __stdcall
+
+#define CONST const
+
+#define DECLARE_HANDLE(name) struct name##__{int unused;}; typedef struct name##__ *name
+
+typedef unsigned int UINT;
+
+typedef long LONG;
+typedef LONG* PLONG;
+
+#ifdef _WIN64
+	typedef __int64 LONG_PTR;
+#else
+	typedef long LONG_PTR;
+#endif
+
+typedef unsigned long DWORD;
+typedef DWORD* LPDWORD;
+
+typedef int BOOL;
+
+typedef char CHAR;
+typedef CHAR* LPSTR;
+typedef CONST CHAR* LPCSTR;
+
+typedef void* LPVOID;
+typedef CONST void* LPCVOID;
+
+DECLARE_HANDLE(HANDLE);
+#define INVALID_HANDLE_VALUE ((HANDLE)(LONG_PTR)-1)
+
+#define GENERIC_READ (0x80000000L)
+#define GENERIC_WRITE (0x40000000L)
+
+#define FILE_SHARE_READ 0x00000001
+#define FILE_SHARE_WRITE 0x00000002
+
+#define FILE_ATTRIBUTE_NORMAL 0x00000080
+
+#define ERROR_FILE_NOT_FOUND 0x00000002
+
+typedef struct SECURITY_ATTRIBUTES* LPSECURITY_ATTRIBUTES;
+typedef struct OVERLAPPED* LPOVERLAPPED;
+
+void WINAPI DebugBreak(void);
+void WINAPI OutputDebugStringA(LPCSTR lpOutputString);
+
+HANDLE WINAPI GetCurrentProcess(void);
+BOOL WINAPI TerminateProcess(HANDLE hProcess, UINT uExitCode);
+
+#define STD_INPUT_HANDLE ((DWORD)-10)
+#define STD_OUTPUT_HANDLE ((DWORD)-11)
+HANDLE WINAPI GetStdHandle(DWORD nStdHandle);
+
+BOOL WINAPI CloseHandle(HANDLE hObject);
+
+#define OPEN_EXISTING 3
+
+HANDLE WINAPI CreateFileA(
+	LPCSTR lpFileName,
+	DWORD dwDesiredAccess,
+	DWORD dwShareMode,
+	LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+	DWORD dwCreationDisposition,
+	DWORD dwFlagsAndAttributes,
+	HANDLE hTemplateFile);
+
+#define FILE_BEGIN 0
+#define FILE_CURRENT 1
+#define FILE_END 2
+
+#define INVALID_SET_FILE_POINTER ((DWORD)-1)
+
+DWORD WINAPI SetFilePointer(
+	HANDLE hFile,
+	LONG lDistanceToMove,
+	PLONG lpDistanceToMoveHigh,
+	DWORD dwMoveMethod);
+
+BOOL WINAPI ReadFile(
+	HANDLE hFile,
+	LPVOID lpBuffer,
+	DWORD nNumberOfBytesToRead,
+	LPDWORD lpNumberOfBytesRead,
+	LPOVERLAPPED lpOverlapped);
+
+BOOL WINAPI WriteFile(
+	HANDLE hFile,
+	LPCVOID lpBuffer,
+	DWORD nNumberOfBytesToWrite,
+	LPDWORD lpNumberOfBytesWritten,
+	LPOVERLAPPED lpOverlapped);
+
+int WINAPI wsprintfA(LPSTR buffer, LPCSTR format, ...);
+int WINAPI wvsprintfA(LPSTR buffer, LPCSTR format, va_list arglist);
+
+DECLARE_HANDLE(HWND);
+
+#define MB_OK 0x00000000L
+
+#define MB_ICONHAND 0x00000010L
+#define MB_ICONERROR MB_ICONHAND
+
+int WINAPI MessageBoxA(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType);
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+/* WINAPI >>> */
+#else /* _WIN32 */
+#error Unsupported platform.
+#endif /* _WIN32 */
+
+#endif /* EAXEFX_APP_PATCHER_CUSTOM_CRT */
+
+/*=========================================================================*/
+
+#ifdef EAXEFX_APP_PATCHER_CUSTOM_CRT
+
+#if defined(_WIN32)
+
+/* <<< C runtime */
+
+#ifdef NDEBUG
+#define assert(x)
+#else
+void assert__(int is_asserted, const char* condition_string, int line, const char* file_name)
+{
+	if (!is_asserted)
+	{
+		return;
+	}
+
+	CHAR message[1024];
+	wsprintfA(message, "%s\n\nat %d\n\nin %s", condition_string, line, file_name);
+	MessageBoxA(NULL, message, "eaxefx_app_patcher", MB_OK | MB_ICONERROR);
+	DebugBreak();
+}
+
+#define assert(x) assert__(!(x), #x, __LINE__, __FILE__)
+#endif
+
+#define CRT_MAX_STREAMS__ 1
+
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
+#define EOF (-1)
+
+#define SEEK_SET 0
+#define SEEK_END 2
+
+typedef struct FILE_s__
+{
+	HANDLE handle;
+} FILE;
+
+static FILE* stdin;
+static FILE crt_stdin__;
+
+static FILE* stdout;
+static FILE crt_stdout__;
+
+static FILE crt_streams__[CRT_MAX_STREAMS__];
+
+#ifdef NDEBUG
+void abort(void)
+{
+	TerminateProcess(GetCurrentProcess(), ERROR_FILE_NOT_FOUND);
+}
+#else
+void crt_abort__(int line, const char* file_name)
+{
+	CHAR message[1024];
+	wsprintfA(message, "Aborted at %d in %s", line, file_name);
+	OutputDebugStringA(message);
+	TerminateProcess(GetCurrentProcess(), ERROR_FILE_NOT_FOUND);
+}
+
+#define abort() crt_abort__(__LINE__, __FILE__)
+#endif
+
+int crt_memcmp__(const void* ptr1, const void* ptr2, size_t num)
+{
+	const unsigned char* ptr1_bytes = (const unsigned char*)ptr1;
+	const unsigned char* ptr2_bytes = (const unsigned char*)ptr2;
+
+	size_t i;
+
+	assert(ptr1 != NULL);
+	assert(ptr2 != NULL);
+
+	for (i = 0; i < num; ++i)
+	{
+		const int diff = ptr1_bytes[i] - ptr2_bytes[i];
+
+		if (diff != 0)
+		{
+			return diff;
+		}
+	}
+
+	return 0;
+}
+
+/* Need this to redefine the intrinsic. */
+#define memcmp crt_memcmp__
+
+int isspace(int c)
+{
+	switch (c)
+	{
+		case ' ':
+		case '\t':
+		case '\n':
+		case '\v':
+		case '\f':
+		case '\r':
+			return 1;
+
+		default:
+			return 0;
+	}
+}
+
+int isdigit(int c)
+{
+	return c >= '0' && c <= '9';
+}
+
+#define CRT_STRTOUL_10_MAX_VALUE__ 0xFFFFFFFFU
+#define CRT_STRTOUL_10_MAX_PREV_VALUE__ (CRT_STRTOUL_10_MAX_VALUE__ / 10U)
+const unsigned long crt_strtoul_10_max_prev_value__ = CRT_STRTOUL_10_MAX_PREV_VALUE__;
+
+#define CRT_STRTOUL_10_MAX_LAST_DIGIT__ (CRT_STRTOUL_10_MAX_VALUE__ - (CRT_STRTOUL_10_MAX_PREV_VALUE__ * 10))
+const unsigned long crt_strtoul_10_max_last_digit__ = CRT_STRTOUL_10_MAX_LAST_DIGIT__;
+
+unsigned long int strtoul(const char* str, char** endptr, int base)
+{
+	unsigned long result = 0;
+
+	assert(str != NULL);
+
+	if (endptr != NULL)
+	{
+		*endptr = (char*)str;
+	}
+
+	if (base == 10)
+	{
+		while (isspace(*str))
+		{
+			str += 1;
+		}
+
+		while (1)
+		{
+			const int c = *str;
+			unsigned long digit;
+
+			if (!isdigit(c))
+			{
+				break;
+			}
+
+			digit = (unsigned long)(c - '0');
+
+			if (result > crt_strtoul_10_max_prev_value__ ||
+				(result == crt_strtoul_10_max_prev_value__ && digit > crt_strtoul_10_max_last_digit__))
+			{
+				/* Overflow. */
+				result = 0;
+				break;
+			}
+
+			result *= 10;
+			result += (unsigned long)(c - '0');
+			str += 1;
+		}
+	}
+	else
+	{
+		assert(0 && "Unsupported radix.");
+	}
+
+	if (endptr != NULL)
+	{
+		*endptr = (char*)str;
+	}
+
+	return result;
+}
+
+int printf(const char* format, ...)
+{
+	int formated_count;
+	CHAR buffer[1024];
+	va_list va_args;
+	DWORD written_count;
+
+	assert(format != NULL);
+
+	va_start(va_args, format);
+	formated_count = wvsprintfA(buffer, format, va_args);
+	va_end(va_args);
+
+	if (formated_count <= 0)
+	{
+		return 0;
+	}
+
+	assert(stdout != NULL);
+	assert(stdout->handle != INVALID_HANDLE_VALUE);
+
+	written_count = 0;
+	WriteFile(stdout->handle, buffer, (DWORD)formated_count, &written_count, NULL);
+	return (int)written_count;
+}
+
+/*-------------------------------------------------------------------------*/
+
+FILE* fopen(const char* filename, const char* mode)
+{
+	size_t i;
+	int is_writable;
+
+	assert(filename != NULL);
+	assert(mode != NULL);
+
+	if (mode[0] == 'r' && mode[1] == 'b' && mode[2] == '\0')
+	{
+		is_writable = 0;
+	}
+	else if (mode[0] == 'r' && mode[1] == '+' && mode[2] == 'b' && mode[3] == '\0')
+	{
+		is_writable = 1;
+	}
+	else
+	{
+		assert(0 && "Unsupported mode.");
+		return NULL;
+	}
+
+	for (i = 0; i < CRT_MAX_STREAMS__; ++i)
+	{
+		if (crt_streams__[i].handle == INVALID_HANDLE_VALUE)
+		{
+			break;
+		}
+	}
+
+	if (i == CRT_MAX_STREAMS__)
+	{
+		assert(0 && "No free stream.");
+		return NULL;
+	}
+
+	crt_streams__[i].handle = CreateFileA(
+		filename,
+		GENERIC_READ | (is_writable ? GENERIC_WRITE : 0),
+		FILE_SHARE_READ | FILE_SHARE_WRITE,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+
+	if (crt_streams__[i].handle == INVALID_HANDLE_VALUE)
+	{
+		return NULL;
+	}
+
+	return &crt_streams__[i];
+}
+
+int fclose(FILE* stream)
+{
+	assert(stream != NULL);
+	assert(stream >= crt_streams__ && stream < &crt_streams__[CRT_MAX_STREAMS__]);
+	assert(stream->handle != INVALID_HANDLE_VALUE);
+
+	if (CloseHandle(stream->handle) == 0)
+	{
+		assert(0 && "Failed to close a handle.");
+		return EOF;
+	}
+
+	stream->handle = INVALID_HANDLE_VALUE;
+	return 0;
+}
+
+int fseek(FILE* stream, long int offset, int origin)
+{
+	DWORD win32_move_method;
+
+	assert(stream != NULL);
+	assert(stream >= crt_streams__ && stream < &crt_streams__[CRT_MAX_STREAMS__]);
+	assert(stream->handle != INVALID_HANDLE_VALUE);
+
+	switch (origin)
+	{
+		case SEEK_SET: win32_move_method = FILE_BEGIN; break;
+		case SEEK_END: win32_move_method = FILE_END; break;
+		default: assert(0 && "Unknown origin."); return EOF;
+	}
+
+	if (SetFilePointer(stream->handle, offset, NULL, win32_move_method) == INVALID_SET_FILE_POINTER)
+	{
+		return EOF;
+	}
+
+	return 0;
+}
+
+long int ftell(FILE* stream)
+{
+	LONG win32_distance_high = 0;
+	LONG win32_distance_low;
+
+	assert(stream != NULL);
+	assert(stream >= crt_streams__ && stream < &crt_streams__[CRT_MAX_STREAMS__]);
+	assert(stream->handle != INVALID_HANDLE_VALUE);
+
+	win32_distance_low = SetFilePointer(stream->handle, 0, &win32_distance_high, FILE_CURRENT);
+
+	if (win32_distance_high != 0 ||
+		win32_distance_low == INVALID_SET_FILE_POINTER ||
+		win32_distance_low > 0xFFFFFFFE)
+	{
+		return EOF;
+	}
+
+	return (long)win32_distance_low;
+}
+
+size_t fread(void* ptr, size_t size, size_t count, FILE* stream)
+{
+	const size_t byte_count = size * count;
+	const DWORD to_read_count = (DWORD)min(byte_count, (size_t)0xFFFFFFFF);
+	DWORD read_count = 0;
+
+	assert(ptr != NULL);
+	assert(stream != NULL);
+	assert(stream == stdin || (stream >= crt_streams__ && stream < &crt_streams__[CRT_MAX_STREAMS__]));
+	assert(stream->handle != INVALID_HANDLE_VALUE);
+
+	if (ReadFile(stream->handle, ptr, to_read_count, &read_count, NULL) == 0)
+	{
+		return 0;
+	}
+
+	return read_count;
+}
+
+size_t fwrite(const void* ptr, size_t size, size_t count, FILE* stream)
+{
+	const size_t byte_count = size * count;
+	const DWORD to_write_count = (DWORD)min(byte_count, (size_t)0xFFFFFFFF);
+	DWORD written_count = 0;
+
+	assert(ptr != NULL);
+	assert(stream != NULL);
+	assert(stream >= crt_streams__ && stream < &crt_streams__[CRT_MAX_STREAMS__]);
+	assert(stream->handle != INVALID_HANDLE_VALUE);
+
+	if (WriteFile(stream->handle, ptr, to_write_count, &written_count, NULL) == 0)
+	{
+		return 0;
+	}
+
+	return written_count;
+}
+
+char* fgets(char* str, int num, FILE* stream)
+{
+	int i = 0;
+
+	assert(str != NULL);
+	assert(num >= 0);
+	assert(stream != NULL);
+	assert(stream == stdin);
+
+	while (i <= num)
+	{
+		if ((i + 1) == num)
+		{
+			return NULL;
+		}
+
+		if (fread(&str[i], 1, 1, stream) == 0)
+		{
+			break;
+		}
+
+		if (str[i] == '\r')
+		{
+		}
+		else if (str[i] == '\n')
+		{
+			i += 1;
+			break;
+		}
+		else
+		{
+			i += 1;
+		}
+	}
+
+	str[i] = '\0';
+	return str;
+}
+
+/*-------------------------------------------------------------------------*/
+
+void crt_startup_std_handle__(DWORD win32_handle, FILE** std_instance, FILE* instance)
+{
+	assert(std_instance != NULL);
+	assert((*std_instance) == NULL);
+	assert(instance != NULL);
+
+	instance->handle = GetStdHandle(win32_handle);
+
+	if (instance->handle == INVALID_HANDLE_VALUE)
+	{
+		abort();
+	}
+
+	(*std_instance) = instance;
+}
+
+void crt_shutdown_std_handle__(FILE** std_instance, FILE* instance)
+{
+	assert(std_instance != NULL);
+	assert((*std_instance) != NULL);
+	*std_instance = NULL;
+	instance->handle = INVALID_HANDLE_VALUE;
+}
+
+void crt_startup_stdin__(void)
+{
+	crt_startup_std_handle__(STD_INPUT_HANDLE, &stdin, &crt_stdin__);
+}
+
+void crt_shutdown_stdin__(void)
+{
+	crt_shutdown_std_handle__(&stdin, &crt_stdin__);
+}
+
+void crt_startup_stdout__(void)
+{
+	crt_startup_std_handle__(STD_OUTPUT_HANDLE, &stdout, &crt_stdout__);
+}
+
+void crt_shutdown_stdout__(void)
+{
+	crt_shutdown_std_handle__(&stdout, &crt_stdout__);
+}
+
+void crt_startup_streams__(void)
+{
+	size_t i;
+
+	for (i = 0; i < CRT_MAX_STREAMS__; ++i)
+	{
+		assert(crt_streams__[i].handle == NULL);
+		crt_streams__[i].handle = INVALID_HANDLE_VALUE;
+	}
+}
+
+void crt_shutdown_streams__(void)
+{
+	size_t i;
+
+	for (i = 0; i < CRT_MAX_STREAMS__; ++i)
+	{
+		BOOL close_handle_result;
+
+		if (crt_streams__[i].handle != INVALID_HANDLE_VALUE)
+		{
+			continue;
+		}
+
+		close_handle_result = CloseHandle(crt_streams__[i].handle);
+		assert(close_handle_result != 0);
+		crt_streams__[i].handle = INVALID_HANDLE_VALUE;
+	}
+}
+
+void crt_startup__(void)
+{
+	crt_startup_stdin__();
+	crt_startup_stdout__();
+	crt_startup_streams__();
+}
+
+void crt_shutdown__(void)
+{
+	crt_shutdown_streams__();
+	crt_shutdown_stdout__();
+	crt_shutdown_stdin__();
+}
+
+/* C runtime >>> */
+
+#else /* _WIN32 */
+#error Unsupported platform.
+#endif /* _WIN32 */
+
+#endif /* EAXEFX_APP_PATCHER_CUSTOM_CRT */
 
 /*=========================================================================*/
 
@@ -904,11 +1524,7 @@ int main(void)
 	int is_cancelled = 0;
 
 	printf("========================================================\n");
-	printf(
-		"EAXEFX Application Patcher v%d.%d.%d\n",
-		EAXEFX_APP_PATCHER_VERSION_MAJOR,
-		EAXEFX_APP_PATCHER_VERSION_MINOR,
-		EAXEFX_APP_PATCHER_VERSION_PATCH);
+	printf("EAXEFX Application Patcher v%s\n", EAXEFX_APP_PATCHER_VERSION);
 	printf("========================================================\n");
 
 	assert(patch_span.count > 0);
@@ -1045,3 +1661,22 @@ int main(void)
 	read_answer();
 	return 0;
 }
+
+#ifdef EAXEFX_APP_PATCHER_CUSTOM_CRT
+
+#if defined(_WIN32)
+
+int main__(void)
+{
+	int result;
+	crt_startup__();
+	result = main();
+	crt_shutdown__();
+	return result;
+}
+
+#else /* _WIN32 */
+#error Unsupported platform.
+#endif /* _WIN32 */
+
+#endif
